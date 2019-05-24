d407f47e330052024c08791286b3daa37c8b9377
许润泽 <xurunze@360.net>
2019-05-24 15:33:04 +0800

    第5题暴力解法（不通过）
    Signed-off-by: 许润泽 <xurunze@360.net>

---
+++ 5.最长回文子串.cpp
@@ -0,0 +1,64 @@
+/*
+ * @lc app=leetcode.cn id=5 lang=cpp
+ *
+ * [5] 最长回文子�?
+ */
+
+#include <stdio.h>
+#include <string>
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+class Solution {
+public:
+    Solution(){
+        maxlen = 0;
+        resault = "";
+        maxresault = "";
+    }
+    string longestPalindrome(string s) {
+        int i;
+        for(i=0;i<s.size()-1;i++)
+        {
+            findlongest(s,i,i);
+            findlongest(s,i,i+1);
+        }
+        return maxresault;
+    }
+    void findlongest(string s,int begin,int end){
+        while(begin >= 0 && end - begin < s.size())
+        {
+            if (s[begin] == s[end])
+            {
+                resault = s.substr(begin,end - begin + 1);
+                begin--;
+                end++;
+            }
+            else
+            {
+                break;
+            }   
+        }
+        if(maxresault.size() <= resault.size())
+        {
+            maxresault = resault;
+        }
+        resault = "";
+    }
+private:
+    int maxlen;
+    string resault;
+    string maxresault;
+};
+
+int main(int args,char* argv[])
+{
+    Solution *A = new Solution;
+    string s("\'\"\"\'");
+    string res;
+    res = A->longestPalindrome(s);
+    cout<<"res = "<<res;
+    return 0;
+}
+
